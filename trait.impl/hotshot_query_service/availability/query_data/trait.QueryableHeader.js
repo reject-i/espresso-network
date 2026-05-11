75ecc52f1d52d33527699f32e04c5996075a382c
reject-i <reject-i@users.noreply.github.com>
2026-05-11 02:40:45 +0000

    deploy: 86e6211ef7dc9a2444a50104db5f3befb194ae83

--- trait.impl/hotshot_query_service/availability/query_data/trait.QueryableHeader.js
+++ trait.impl/hotshot_query_service/availability/query_data/trait.QueryableHeader.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_query_service/availability/query_data/trait.QueryableHeader.html\" title=\"trait hotshot_query_service::availability::query_data::QueryableHeader\">QueryableHeader</a>&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"enum\" href=\"espresso_types/v0/enum.Header.html\" title=\"enum espresso_types::v0::Header\">Header</a>",0]]],["hotshot_query_service",[]]]);
+    const implementors = Object.fromEntries([["espresso_types",[["impl QueryableHeader&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"enum\" href=\"espresso_types/v0/enum.Header.html\" title=\"enum espresso_types::v0::Header\">Header</a>",0]]],["hotshot_query_service",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[486,29]}
+//{"start":59,"fragment_lengths":[300,29]}
