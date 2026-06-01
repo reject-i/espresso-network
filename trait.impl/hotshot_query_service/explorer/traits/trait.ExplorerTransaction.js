4ae60e2e2d452a976763449f28c5896ae7566972
reject-i <reject-i@users.noreply.github.com>
2026-06-01 03:31:06 +0000

    deploy: 86e6211ef7dc9a2444a50104db5f3befb194ae83

--- trait.impl/hotshot_query_service/explorer/traits/trait.ExplorerTransaction.js
+++ trait.impl/hotshot_query_service/explorer/traits/trait.ExplorerTransaction.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_query_service/explorer/traits/trait.ExplorerTransaction.html\" title=\"trait hotshot_query_service::explorer::traits::ExplorerTransaction\">ExplorerTransaction</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::Transaction\">Transaction</a>",0]]],["hotshot_query_service",[]]]);
+    const implementors = Object.fromEntries([["espresso_types",[["impl ExplorerTransaction for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::Transaction\">Transaction</a>",0]]],["hotshot_query_service",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[380,29]}
+//{"start":59,"fragment_lengths":[202,29]}
