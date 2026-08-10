0100909822cd80fb870e1622de4a200a3b7e1ad8
reject-i <reject-i@users.noreply.github.com>
2026-08-10 01:18:32 +0000

    deploy: 86e6211ef7dc9a2444a50104db5f3befb194ae83

--- trait.impl/request_response/network/trait.Sender.js
+++ trait.impl/request_response/network/trait.Sender.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["request_response",[]],["sequencer",[["impl <a class=\"trait\" href=\"request_response/network/trait.Sender.html\" title=\"trait request_response::network::Sender\">Sender</a>&lt;VerKey&gt; for <a class=\"struct\" href=\"sequencer/request_response/network/struct.Sender.html\" title=\"struct sequencer::request_response::network::Sender\">Sender</a>",0]]]]);
+    const implementors = Object.fromEntries([["request_response",[]],["sequencer",[["impl Sender&lt;VerKey&gt; for <a class=\"struct\" href=\"sequencer/request_response/network/struct.Sender.html\" title=\"struct sequencer::request_response::network::Sender\">Sender</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[23,333]}
+//{"start":59,"fragment_lengths":[23,208]}
